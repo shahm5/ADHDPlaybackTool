849f214ca8ad689623d9fd81902e252f9e2460ad
ManasiShah1 <shahm5@cs.washington.edu>
2020-10-17 16:01:16 -0700

    set up basic layout

--- adhd-playback-tool/src/App.js
+++ adhd-playback-tool/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { Video } from './Video';
+import { HomePage } from './HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact componenent={HomePage}/>
+        <Route path='/home' exact component={HomePage}/>
+        <Route path='/video' exact component={Video}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
