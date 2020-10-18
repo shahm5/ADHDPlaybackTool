c09cb23d02c0117c845523af813e87fc9fcc6fe3
ManasiShah1 <shahm5@cs.washington.edu>
2020-10-17 18:22:47 -0700

    embed video given link

--- adhd-playback-tool/src/Video.js
+++ adhd-playback-tool/src/Video.js
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./styles/Video.css";
 
 export const Video = () => {
-    return <p>video</p>
-}
+  const [url, setUrl] = useState("");
+  const [urlInput, setInput] = useState("");
+  const pauseInterval = 5;
+
+  const updateInput = (e) => {
+    setInput(e.target.value);
+  };
+  const getVideo = () => {
+    let urlBuilder =
+      urlInput.substring(0, 24) + "embed/" + urlInput.substr(24 + 8);
+    setUrl(urlBuilder);
+  };
+
+  return (
+    <div className="videoComp">
+      <input onChange={updateInput} value={urlInput} />
+      <button onClick={getVideo}>Get my video!</button>
+      {url === "" ? (
+        <div />
+      ) : (
+        <div>
+          <iframe
+            width="900"
+            height="506"
+            src={url}
+            frameborder="0"
+            allowfullscreen
+            className="video"
+          ></iframe>
+        </div>
+      )}
+    </div>
+  );
+};
